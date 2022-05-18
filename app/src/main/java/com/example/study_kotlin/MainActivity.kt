a4c4a3a521de096fce9cb3db8d037f7930acfad5
sub1nyun <97420319+sub1nyun@users.noreply.github.com>
2022-05-19 02:13:27 +0900

    수빈

--- app/src/main/java/com/example/study_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/study_kotlin/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.study_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.study_kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //gradle 에 viewBinding 추가
+    //private var mBinding : ActivityMainBinding? = null // 바인딩 객체 선언
+    private var mBinding : ActivityMainBinding? = null
+    //구현부에서 null safety로 처리할건지 아닌지 계속 물어봄\
+    //null 발생시 nullPointerException을 발생 시키려고
+    private val binding get() = mBinding!!  //null 체크
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        viewBinding()
+
+        btnclick()
+
+        //var a : String? = null
+        lateinit var a : String
+
+        binding.btn2.setOnClickListener {
+            binding.text2.text = "머임"
+
+        }
     }
+    private fun viewBinding() : Unit{
+        mBinding = ActivityMainBinding.inflate(layoutInflater) //xml 붙힘
+        setContentView(binding.root) //생선한 루트 ㅂ ㅠ 붙힘
+        //같은 layout id 넘겨주면 inflater 두 번 발생함
+        //xml을 감싸는 최상단 부모를 root라는 property로 제공
+        //반드시 binding 객체의 root를 넘겨줘야함
+    }
+
+
+
+    private fun btnclick() : Unit {
+        binding.btn.setOnClickListener {
+            binding.text.text = "가고싶다고 집에"
+
+            binding.btn2.setOnClickListener {
+                binding.text2.text = "이거 여기에 추가하면 생김?"
+            }
+        }
+    }
+
+
 }
+
+
---
+++ app/src/main/java/com/example/study_kotlin/Sample2.kt
@@ -0,0 +1,69 @@
+package com.example.study_kotlin
+
+fun main() {
+    checkNum(1)
+
+}
+
+//1. 조건식
+fun maxBy(a : Int, b : Int) : Int {
+    if(a>b) {
+        return a
+    }else {
+        return b
+    }
+}
+//다른 방식 (위와 같음)
+fun maxBy2(a : Int, b : Int) = if(a>b) a else b
+//코틀린 3항 연산자가 없음 -> 위처럼 사용
+
+fun checkNum(score : Int) {//: Unit { //문장으로 쓰였음
+    when(score) { //when -> switch와 같음
+        0 -> println("this is 0")
+        1 -> println("this is 1")
+        2,3 -> println("this is 2 or 3") //복수로 조건 인정
+        else -> println("I don't know") //function의 경우 else 생략 가능
+    }
+
+    //when 다른 방식
+    var b = when(score) { //Expression 값
+        1-> 1
+        2-> 2
+        //리턴식으로 사용 가능 -> 반드시 else를 적용해야함
+        else -> 3
+    }
+
+    println("b : ${b}")
+
+    when(score){
+        in 90..100 -> println("90~100")
+        in 10..80 -> println("10~80")
+        else -> println("80~90")
+    }
+
+    lateinit var text : String
+    //나중에 초기화
+
+    var a : String? = null
+    //null 지양 -> Kotlin
+
+    lateinit var aa : String
+
+    var test : Int = 30
+
+    aa = "test : ${test}"
+    println(aa)
+
+    var test2 : Int = 50
+
+    aa = "test : ${test + test2}"
+    println(aa)
+
+    println("${1==0}")
+
+
+}
+
+// Expression vs Statement -> 값 : 문장의 느낌 
+//코틀린 함수는 Expression : Unit -> 리턴 값이 없더라도 Unit을 반환함
+//자바의 void의 경우 Statement
